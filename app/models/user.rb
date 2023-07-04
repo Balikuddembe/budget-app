ac308a083e22213074017f8b78fc7027e904b137
Balikuddembe <balixemma@gmail.com>
2023-07-04 20:16:55 +0300

    Add user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     # associations
+    has_many :groups, class_name: 'Group', dependent: :destroy, foreign_key: 'user_id'
+    has_many :expenses, class_name: 'Expense', dependent: :destroy, foreign_key: 'user_id'
 
     # validations
     validates :name, presence: true
