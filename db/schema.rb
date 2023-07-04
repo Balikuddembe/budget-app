df25e4bc74a2e06b2067730262a663947e96df60
Balikuddembe <balixemma@gmail.com>
2023-07-04 19:40:23 +0300

    Run rails migration for expense model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_160744) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_163339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
