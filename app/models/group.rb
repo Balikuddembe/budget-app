b87f6f49ee3599e1f918c590220f65a4c2f53d26
Balikuddembe <balixemma@gmail.com>
2023-07-04 22:24:28 +0300

    Fix rubocop

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ApplicationRecord
-    # associations
-    has_many :group_expenses, dependent: :destroy
-    has_many :expenses, through: :group_expenses
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  # associations
+  has_many :group_expenses, dependent: :destroy
+  has_many :expenses, through: :group_expenses
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-    # validations
-    validates :name, presence: true, length: { in: 1..25 }
-    validates :icon, presence: true
+  # validations
+  validates :name, presence: true, length: { in: 1..25 }
+  validates :icon, presence: true
 end
