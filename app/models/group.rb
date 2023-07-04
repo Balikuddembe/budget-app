0f4a49b18de792243b56e54b0f826e565cf1f247
Balikuddembe <balixemma@gmail.com>
2023-07-04 20:39:48 +0300

    Add associations to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,8 @@
 class Group < ApplicationRecord
     # associations
+    has_many :group_expenses, dependent: :destroy
+    has_many :expenses, through: :group_expenses
+    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
     # validations
     validates :name, presence: true, length: {in 1..25}
